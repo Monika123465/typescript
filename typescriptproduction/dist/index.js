1dba8fafaa4e18a737e970b0dbebd80399598a34
Monika kumari <105919665+Monika123465@users.noreply.github.com>
2024-04-15 22:21:29 +0530

    add typescript

---
+++ typescriptproduction/dist/index.js
@@ -0,0 +1,8 @@
+"use strict";
+class User {
+    constructor(email, name) {
+        this.email = email,
+            this.name = name;
+    }
+}
+const monika = new User('monika@tm', 'monika');
---
+++ typescriptproduction/src/index.ts
@@ -0,0 +1,10 @@
+class User{
+    email:string
+    name:string
+    constructor(email:string,name:string){
+
+        this.email=email,
+        this.name=name
+    }}
+
+    const monika=new User('monika@tm','monika')
